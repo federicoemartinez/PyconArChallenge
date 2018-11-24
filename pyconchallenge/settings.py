b812859e8baa456cd0b54692d730a8e31b5b9b98
santiavenda2 <santiago.avendano@invgate.com>
2018-11-24 14:34:33 -0300

    Agrego ganadores

--- pyconchallenge/settings.py
+++ pyconchallenge/settings.py
@@ -76,17 +76,22 @@ WSGI_APPLICATION = 'pyconchallenge.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pyconchallenge',  # Or path to database file if using sqlite3.
-        'USER': 'pyconchallenge',  # Not used with sqlite3.
-        'PASSWORD': 'INV4ever',  # Not used with sqlite3.
-        'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'pyconchallenge',  # Or path to database file if using sqlite3.
+#        'USER': 'pyconchallenge',  # Not used with sqlite3.
+#        'PASSWORD': 'INV4ever',  # Not used with sqlite3.
+#        'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 
 
 # Password validation
